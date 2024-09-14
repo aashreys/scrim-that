2c4af475be0fda5b387476d69b7973e43265474c
Aashrey Sharma <aashrey9sharma@gmail.com>
2024-09-14 12:43:39 -0700

    Rewrote scrimming logic to handle autolayout, top level frames and groups and general layers. Removed explicit support for shape layer scrimming.

--- src/main.ts
+++ src/main.ts
@@ -1,66 +1,110 @@
-const NOTI_TIME_LONG = 5000;
+const SCRIM_FILL: [SolidPaint] = [{
+  type: 'SOLID',
+  color: { r: 0, g: 0, b: 0 },
+  opacity: 0.25
+}]
 
 export default function () {
-  let selection = figma.currentPage.selection;
+  let selection = figma.currentPage.selection
   if (selection.length > 1) {
-    showError('More than one layer selected. Please select a single layer to scrim.', NOTI_TIME_LONG)
+    figma.notify('🔴 Multiple layers selected. Please select a single layer to scrim.')
   } else if (selection.length === 1) {
     processSelection(selection[0])
+    figma.notify('Scrim created 🎉')
   } else {
-    showError('Nothing selected. Please select a layer to scrim.', NOTI_TIME_LONG)
+    figma.notify('🔴 Nothing selected. Please select a layer to scrim.')
   }
   figma.closePlugin()
 }
 
 function processSelection(layer: SceneNode) {
-  if (isFrame(layer)) {
-    scrimFrame(layer as FrameNode)
-  } else if (isGroup(layer)) {
-    scrimGroup(layer as GroupNode)
-  } else if (isShape(layer)) {
-    scrimShape(layer)
-  } else {
-    showError('Unsupported layer. Please select a Frame, Group or Shape layer to scrim.', NOTI_TIME_LONG)
+  if (isTopLevelLayer(layer)) {
+    if (isAutoLayout(layer)) scrimAutoLayout(layer as FrameNode)
+    else if (isFrame(layer)) scrimFrame(layer as FrameNode)
+    else if (isGroup(layer)) scrimGroup(layer as GroupNode)
+    else scrimLayer(layer as SceneNode)
+  }
+  else {
+    scrimLayer(layer)
   }
 }
 
-function scrimFrame(container: FrameNode) {
+function scrimAutoLayout(autoLayout: FrameNode) {
+  const scrim = figma.createRectangle()
+  scrim.name = 'Scrim'
+  scrim.fills = createScrimFill()
+  scrim.resize(autoLayout.width, autoLayout.height)
+  scrim.cornerSmoothing = autoLayout.cornerSmoothing
+  scrim.topLeftRadius = autoLayout.topLeftRadius
+  scrim.topRightRadius = autoLayout.topRightRadius
+  scrim.bottomLeftRadius = autoLayout.bottomLeftRadius
+  scrim.bottomRightRadius = autoLayout.bottomRightRadius
+
+  scrim.constraints = {
+    horizontal: 'SCALE',
+    vertical: 'SCALE'
+  }
+
+  autoLayout.itemReverseZIndex ? autoLayout.insertChild(0, scrim) : autoLayout.appendChild(scrim)
+
+  scrim.layoutPositioning = 'ABSOLUTE'
+}
+
+function scrimFrame(frame: FrameNode) {
+  const scrim = figma.createRectangle()
+  scrim.name = 'Scrim'
+  scrim.fills = createScrimFill()
+  scrim.resize(frame.width, frame.height)
+  scrim.cornerSmoothing = frame.cornerSmoothing
+  scrim.topLeftRadius = frame.topLeftRadius
+  scrim.topRightRadius = frame.topRightRadius
+  scrim.bottomLeftRadius = frame.bottomLeftRadius
+  scrim.bottomRightRadius = frame.bottomRightRadius
+
+  scrim.constraints = {
+    horizontal: 'SCALE',
+    vertical: 'SCALE'
+  }
+
+  frame.appendChild(scrim)
+}
+
+function scrimGroup(group: GroupNode) {
   let scrim = figma.createRectangle()
   scrim.name = 'Scrim'
-  scrim.fills = [createScrimFill()]
-  scrim.x = container.x;
-  scrim.y = container.y;
-  scrim.resize(container.width, container.height)
-  scrim.cornerSmoothing = (container as any).cornerSmoothing
-  scrim.topLeftRadius = (container as any).topLeftRadius
-  scrim.topRightRadius = (container as any).topRightRadius
-  scrim.bottomLeftRadius = (container as any).bottomLeftRadius
-  scrim.bottomRightRadius = (container as any).bottomRightRadius
-  container.parent?.insertChild(container.parent.children.indexOf(container) + 1, scrim)
+  scrim.fills = createScrimFill()
+  scrim.x = group.x;
+  scrim.y = group.y;
+  scrim.resize(group.width, group.height)
+  
+  group.appendChild(scrim)
 }
 
-function scrimGroup(container: GroupNode) {
-  let scrim = figma.createRectangle()
+function scrimLayer(layer: SceneNode) {
+  const scrim = figma.createRectangle()
   scrim.name = 'Scrim'
-  scrim.fills = [createScrimFill()]
-  scrim.x = container.x;
-  scrim.y = container.y;
-  scrim.resize(container.width, container.height)
-  container.parent?.insertChild(container.parent.children.indexOf(container) + 1, scrim)
+  scrim.fills = createScrimFill()
+  scrim.resize(layer.width, layer.height)
+  scrim.x = layer.x
+  scrim.y = layer.y
+
+  layer.parent?.insertChild(layer.parent.children.indexOf(layer) + 1, scrim)
 }
 
-function scrimShape(shape: SceneNode) {
-  let scrim: any = shape.clone()
-  scrim.name = 'Scrim'
-  scrim.fills = [createScrimFill()]
-}
-
-function createScrimFill(): SolidPaint {
-  return {
+function createScrimFill(): [SolidPaint] {
+  return [{
     type: 'SOLID',
     color: { r: 0, g: 0, b: 0 },
     opacity: 0.25
-  }
+  }]
+}
+
+function isTopLevelLayer(layer: SceneNode): boolean {
+  return layer.parent?.type === 'PAGE'
+}
+
+function isAutoLayout(node: SceneNode): boolean {
+  return node.type === 'FRAME' && (node.layoutMode === 'HORIZONTAL' || node.layoutMode === 'VERTICAL')
 }
 
 function isFrame(node: SceneNode): boolean {
@@ -69,19 +113,4 @@ function isFrame(node: SceneNode): boolean {
 
 function isGroup(node: SceneNode): boolean {
   return node.type === 'GROUP'
-}
-
-function isShape(node: SceneNode): boolean {
-  return node.type === 'ELLIPSE' ||
-  node.type === 'POLYGON' ||
-  node.type === 'RECTANGLE' ||
-  node.type === 'STAR' ||
-  node.type === 'VECTOR'
-}
-
-function showError(message: string, time: number) {
-  figma.notify(
-    message,
-    { timeout: time }
-  )
 }
